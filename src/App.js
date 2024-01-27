ef88e446c7fdbe39abe194b9ebfebe2139f4d3dc
Suraj Jha <sarcasticsuraj7048@gmail.com>
2024-01-27 19:47:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
 
-function App() {
+const App = () => {
+  const [message,setMessage] = useState(null);
+  const [value,setValue] = useState("");
+  const [previousChats,setPreviousChats] = useState([]);
+  const [currentTitle,setCurrentTitle] = useState(null);
+
+  const createNewChat = () => {
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(null);
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue("");
+  }
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    try{
+      const response = await fetch('http://localhost:8000/completions',options);
+      const data = await response.json();
+      console.log(data);
+      setMessage(data.choices[0].message);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+  
+  //console.log(message);
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+
+    if(!currentTitle && value && message){
+      setCurrentTitle(value);   
+    }
+    
+    if(currentTitle && value && message){
+      setPreviousChats(prevChats => (
+        [...prevChats,
+        {
+          title: currentTitle,
+          role: "user",
+          content:value
+        },
+        {
+          title: currentTitle,
+          role:message.role,
+          content:message.content
+        }]
+      ))
+    }
+
+  },[message,currentTitle]);
+
+  const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle);
+  const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)));
+  console.log(uniqueTitles);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <section className="side-bar">
+       <button onClick={createNewChat}>➕ New chat</button>
+       <ul className="history">
+         {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
+       </ul>
+       <nav>
+         <p>Made with ❤️ by Suraj Jha</p>
+       </nav>
+     </section>
+
+     <section className="main">
+       {!currentTitle && <h1>SurajGPT</h1>}
+       
+       <ul className="feed">
+         {currentChat.map((chatMessage, index) => <li key={index}>
+            <p className="role">{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+         </li>)}
+       </ul>
+       <div className="bottom-section">
+         <div className="input-container">
+           <input value={value} onChange={(e) => setValue(e.target.value)}/>
+           <div id="submit" onClick={getMessages}>➢</div>
+         </div>
+
+         <p className="info">
+           Suraj GPT Jan 2024 Version. Free Research Preview.
+           Our goal is to make AI systems more natural and safe to interact with.
+           Your feedback will help us improve.
+         </p>
+
+       </div>
+     </section>
     </div>
   );
 }
